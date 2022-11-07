c01e4e995e8f8bd694cedb29c3bbe7c4c4cf6421
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2022-11-07 12:24:05 +0530

    initial commit

---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     quizaccess_examproctoring
+ * @copyright   2022 Shevan Thiranja Fernando <w.k.b.s.t.fernando@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'quizaccess_examproctoring';
+$plugin->release = '0.1.0';
+$plugin->version = 2022110700;
+$plugin->requires = 2021051700;
+$plugin->maturity = MATURITY_ALPHA;
