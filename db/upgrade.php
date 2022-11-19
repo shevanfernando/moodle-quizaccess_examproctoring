44518cd3eb3f2e7715d0226bca2e845cb5c98ff5
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2022-11-19 13:25:53 +0530

    added upgrade.php to update plugin data bases

---
+++ db/upgrade.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Implementation of the quizaccess_examproctoring plugin.
+ *
+ * @package    quizaccess_examproctoring
+ * @copyright  2022 Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+function xmldb_qtype_myqtype_upgrade($oldversion)
+{
+    global $DB;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2022111900) {
+
+        // Define table quizaccess_examproctoring to be created.
+        $table = new xmldb_table('quizaccess_examproctoring');
+
+        // Adding fields to table quizaccess_examproctoring.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('quizid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('webcamproctoringrequired', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('screenproctoringrequired', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0');
+
+        // Adding keys to table quizaccess_examproctoring.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('quizid', XMLDB_KEY_FOREIGN_UNIQUE, ['quizid'], 'quiz', ['id']);
+
+        // Conditionally launch create table for quizaccess_examproctoring.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Examproctoring savepoint reached.
+        upgrade_plugin_savepoint(true, 2022111900, 'quizaccess', 'examproctoring');
+    }
+
+    return true;
+}
