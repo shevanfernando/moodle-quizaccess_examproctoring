d15e627df0fe88e0c0417d94314d56a09404d754
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2022-11-20 17:08:22 +0530

    update quizaccess_examproctoring.php rename plugin name

--- lang/en/quizaccess_examproctoring.php
+++ lang/en/quizaccess_examproctoring.php
@@ -17,7 +17,7 @@
 /**
  * Plugin strings are defined here.
  *
- * @package     quizaccess_examproctoring
+ * @package     quizaccess_exproctor
  * @category    string
  * @copyright   2022 Shevan Thiranja Fernando <w.k.b.s.t.fernando@gmail.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Exam Proctoring';
-$string['examproctoring:send_evidence'] = 'Send evidence files';
-$string['examproctoring:get_evidence'] = 'Read evidence files';
-$string['examproctoring:view_report'] = 'View proctoring report';
-$string['examproctoring:delete_evidence'] = 'Delete evidence files';
+$string['exproctor:send_evidence'] = 'Send evidence files';
+$string['exproctor:get_evidence'] = 'Read evidence files';
+$string['exproctor:view_report'] = 'View proctoring report';
+$string['exproctor:delete_evidence'] = 'Delete evidence files';
 $string['settings:storage_method'] = 'Data storage method';
 $string['settings:storage_method_description'] = 'Proctored data storage method. (Values: Local/ AWS(S3))';
 $string['settings:local_storage_path'] = 'Local storage folder path';
@@ -41,7 +41,9 @@ $string['settings:aws_access_id_description'] = 'AWS access key id for S3 bucket
 $string['settings:aws_access_key'] = 'AWS secret access key';
 $string['settings:aws_access_key_description'] = 'AWS secret access key for S3 bucket.';
 $string['openwebcam'] = 'Allow your webcam to continue';
-$string['proctoringstatement'] = 'This exam requires webcam validation process. You must allow the webcam and it will take some random pictures during the exam. <br />(Please allow your web browser to access your camera).';
+$string['openscreen'] = 'Chose what to share';
+$string['webcamproctoringstatement'] = 'This exam requires webcam validation process. <br />(Please allow your web browser to access your camera).';
+$string['screenproctoringstatement'] = 'This exam requires screen validation process. <br />(Please select screen to share).';
 $string['camhtml'] = '<div class="camera"> <video id="video">Video stream not available.</video></div> <canvas id="canvas" style="display:none;"> </canvas> <img style="display:none;" id="photo" alt="The screen capture will appear in this box."/>';
 $string['proctoringlabel'] = 'I agree with the validation process.';
 $string['youmustagree'] = 'You must agree to validate your identity before continue.';
