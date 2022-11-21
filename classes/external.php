195230088c479be970dfecd01a742b69b64bd035
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2022-11-21 11:50:18 +0530

    update external.php to add screen shots

--- classes/external.php
+++ classes/external.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+global $CFG;
+
 defined('MOODLE_INTERNAL') || die;
 
 require_once("$CFG->libdir/externallib.php");
@@ -268,5 +270,223 @@ class quizaccess_exproctor_external extends external_api
         if ($USER->id != $user->id) {
             require_capability('quizaccess/examproctoring:view_report', $context);
         }
+    }
+
+    public static function send_screen_shot($courseid, $screenshotid, $quizid, $screenpicture): array
+    {
+        global $DB, $USER;
+
+        // Validate the params
+        self::validate_parameters(
+            self::send_screen_shot_parameters(),
+            array(
+                'courseid' => $courseid,
+                'screenshotid' => $screenshotid,
+                'quizid' => $quizid,
+                'screenpicture' => $screenpicture,
+            )
+        );
+
+        $warnings = array();
+
+        $record = new stdClass();
+        $record->filearea = 'picture';
+        $record->component = 'quizaccess_exproctor';
+        $record->filepath = '';
+        $record->itemid = $screenshotid;
+        $record->license = '';
+        $record->author = '';
+
+        $context = context_module::instance($quizid);
+        $fs = get_file_storage();
+        $record->filepath = file_correct_filepath($record->filepath);
+
+        // For base64 to file.
+        $data = $screenpicture;
+        list($type, $data) = explode(';', $data);
+        list(, $data) = explode(',', $data);
+        $data = base64_decode($data);
+        $filename = 'screen-' . $screenshotid . '-' . $USER->id . '-' . $courseid . '-' . time() . rand(1, 1000) . '.png';
+
+        $record->courseid = $courseid;
+        $record->filename = $filename;
+        $record->contextid = $context->id;
+        $record->userid = $USER->id;
+
+        $file = $fs->create_file_from_string($record, $data);
+
+        $filesql = 'SELECT * FROM {files} WHERE userid IN (' . $USER->id . ') AND contextid IN (' . $context->id . ') AND mimetype = \'image/png\' AND component = \'quizaccess_exproctor\' AND filearea = \'picture\' ORDER BY id DESC LIMIT 1';
+
+        $usersfile = $DB->get_records_sql($filesql);
+
+        $file_id = 0;
+        foreach ($usersfile as $tempfile):
+            $file_id = $tempfile->id;
+        endforeach;
+
+        $url = moodle_url::make_pluginfile_url(
+            $context->id,
+            $record->component,
+            $record->filearea,
+            $record->itemid,
+            $record->filepath,
+            $record->filename,
+            false
+        );
+
+        $camshot = $DB->get_record('quizaccess_exproctor_sc_logs', array('id' => $screenshotid));
+
+        $record = new stdClass();
+        $record->courseid = $courseid;
+        $record->quizid = $quizid;
+        $record->userid = $USER->id;
+        $record->screenpicture = "{$url}";
+        $record->status = $camshot->status;
+        $record->fileid = $file_id;
+        $record->timemodified = time();
+        $screenshotid = $DB->insert_record('quizaccess_exproctor_sc_logs', $record, true);
+
+        $result = array();
+        $result['screenshotid'] = $screenshotid;
+        $result['warnings'] = $warnings;
+        return $result;
+    }
+
+    /**
+     * Store parameters
+     *
+     * @return external_function_parameters
+     */
+    public static function send_screen_shot_parameters(): external_function_parameters
+    {
+        return new external_function_parameters(
+            array(
+                'courseid' => new external_value(PARAM_INT, 'course id'),
+                'screenshotid' => new external_value(PARAM_INT, 'screen shot id'),
+                'quizid' => new external_value(PARAM_INT, 'quiz id'),
+                'screenpicture' => new external_value(PARAM_RAW, 'screen picture'),
+            )
+        );
+    }
+
+    /**
+     * Webcam shots return parameters.
+     *
+     * @return external_single_structure
+     */
+    public static function send_screen_shot_returns(): external_single_structure
+    {
+        return new external_single_structure(
+            array(
+                'screenshotid' => new external_value(PARAM_INT, 'screen shot sent id'),
+                'warnings' => new external_warnings()
+            )
+        );
+    }
+
+    /**
+     * Webcam shot return parameters.
+     *
+     * @return external_single_structure
+     */
+    public static function get_screen_shot_returns(): external_single_structure
+    {
+        return new external_single_structure(
+            array(
+                'screenshots' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'courseid' => new external_value(PARAM_NOTAGS, 'screen shot course id'),
+                            'quizid' => new external_value(PARAM_NOTAGS, 'screen shot quiz id'),
+                            'userid' => new external_value(PARAM_NOTAGS, 'screen shot user id'),
+                            'screenpicture' => new external_value(PARAM_RAW, 'screen shot photo'),
+                            'timemodified' => new external_value(PARAM_NOTAGS, 'screen shot time modified'),
+                        )
+                    ),
+                    'list of screenshots'
+                ),
+                'warnings' => new external_warnings()
+            )
+        );
+    }
+
+    /**
+     * Get the screen shots as service.
+     *
+     * @param mixed $courseid course id.
+     * @param mixed $quizid context/quiz id.
+     * @param mixed $userid user id.
+     *
+     * @return array
+     * @throws dml_exception
+     * @throws invalid_parameter_exception
+     * @throws moodle_exception
+     * @throws required_capability_exception
+     */
+    public static function get_screen_shot($courseid, $quizid, $userid): array
+    {
+        global $DB, $USER;
+
+        $params = array(
+            'courseid' => $courseid,
+            'quizid' => $quizid,
+            'userid' => $userid
+        );
+
+        // Validate the params.
+        self::validate_parameters(self::get_screen_shot_parameters(), $params);
+
+        $context = context_module::instance($params['quizid']);
+
+        // Default value for userid.
+        if (empty($params['userid'])) {
+            $params['userid'] = $USER->id;
+        }
+
+        self::request_user_require_capability($params, $context, $USER);
+
+        $warnings = array();
+        if ($params['quizid']) {
+            $camshots = $DB->get_records('quizaccess_exproctor_sc_logs', $params, 'id DESC');
+        } else {
+            $camshots = $DB->get_records('quizaccess_exproctor_sc_logs',
+                array('courseid' => $courseid, 'userid' => $userid), 'id DESC');
+        }
+
+        $returnedscreenhosts = array();
+
+        foreach ($camshots as $camshot) {
+            if ($camshot->screenpicture !== '') {
+                $returnedscreenhosts[] = array(
+                    'courseid' => $camshot->courseid,
+                    'quizid' => $camshot->quizid,
+                    'userid' => $camshot->userid,
+                    'screenpicture' => $camshot->screenpicture,
+                    'timemodified' => $camshot->timemodified,
+                );
+
+            }
+        }
+
+        $result = array();
+        $result['screenshots'] = $returnedscreenhosts;
+        $result['warnings'] = $warnings;
+        return $result;
+    }
+
+    /**
+     * Set the cam shots parameters.
+     *
+     * @return external_function_parameters
+     */
+    public static function get_screen_shot_parameters(): external_function_parameters
+    {
+        return new external_function_parameters(
+            array(
+                'courseid' => new external_value(PARAM_INT, 'screen shot course id'),
+                'quizid' => new external_value(PARAM_INT, 'screen shot quiz id'),
+                'userid' => new external_value(PARAM_INT, 'screen shot user id')
+            )
+        );
     }
 }
