265e91f68825beb819d201db90e98087909400b8
Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
2023-02-04 17:56:52 +0530

    implement aws_s3 class to handle s3 bucket functions

---
+++ classes/aws_s3.php
@@ -0,0 +1,186 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * quizaccess_exproctor file description here.
+ *
+ * @package    quizaccess_exproctor
+ * @copyright  2022 Shevan Fernando <w.k.b.s.t.fernando@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace quizaccess_exproctor;
+
+global $CFG;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/quiz/accessrule/exproctor/aws_sdk/aws-autoloader.php');
+
+use Aws\Exception\AwsException;
+use Aws\Result;
+use Aws\S3\S3Client;
+
+class aws_s3
+{
+    private $s3Client;
+    private $data;
+
+    /**
+     * Constructor
+     *
+     * @throws \dml_exception
+     */
+    public function __construct()
+    {
+        global $DB;
+
+        $records = $DB->get_records("config_plugins", ['plugin' => 'quizaccess_exproctor'], 'id', 'name, value');
+
+        foreach ($records as $elements) {
+            $this->data[$elements->name] = $elements->value;
+        }
+
+        $this->s3Client = new S3Client([
+            'version'     => 'latest',
+            'region'      => $this->data['awsregion'],
+            'credentials' => [
+                'key'    => $this->data['awsaccesskey'],
+                'secret' => $this->data['awssecretkey']
+            ]
+        ]);
+    }
+
+    /**
+     * Get S3 Client
+     *
+     * @return mixed
+     */
+    public function getS3Client()
+    {
+        return $this->s3Client;
+    }
+
+    /**
+     * Get exproctor settings array
+     *
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     *  Delete S3 Bucket
+     *
+     * @param $bucketName
+     * @return mixed|string|null
+     */
+    public function deleteBucket($bucketName)
+    {
+        try {
+            // Delete the objects in the bucket before attempting to delete
+            // the bucket
+            $iterator = $this->s3Client->getIterator('ListObjects', array(
+                'Bucket' => $bucketName
+            ));
+
+            foreach ($iterator as $object) {
+                $this->deleteImage($bucketName, $object['Key']);
+            }
+
+            // Delete the bucket
+            $this->s3Client->deleteBucket(array('Bucket' => $bucketName));
+
+            // Wait until the bucket is not accessible
+            $this->s3Client->waitUntil('BucketNotExists', array('Bucket' => $bucketName));
+
+            return true;
+        } catch (AwsException $e) {
+            return 'Error: ' . $e->getAwsErrorMessage();
+        }
+    }
+
+    /**
+     * Delete Image
+     *
+     * @param $bucketName
+     * @param $fileName
+     * @return Result|string
+     */
+    public function deleteImage($bucketName, $fileName)
+    {
+        try {
+            // Delete image
+            return $this->s3Client->deleteObject([
+                'Bucket' => $bucketName,
+                'Key'    => $fileName
+            ]);
+        } catch (AwsException $e) {
+            return 'Error: ' . $e->getAwsErrorMessage();
+        }
+    }
+
+    /**
+     * Create S3 bucket
+     *
+     * @return string
+     */
+    public function createBucket(): string
+    {
+        try {
+            # Create the bucket name
+            $bucketName = md5(time());;
+
+            // Create the bucket
+            $this->s3Client->createBucket([
+                'Bucket' => $bucketName,
+            ]);
+
+            // Poll the bucket until it is accessible
+            $this->s3Client->waitUntil('BucketExists', array('Bucket' => $bucketName));
+
+            return $bucketName;
+        } catch (AwsException $e) {
+            return 'Error: ' . $e->getAwsErrorMessage();
+        }
+    }
+
+    /**
+     *  Save images
+     *
+     * @param $bucketName
+     * @param $imageData
+     * @param $fileName
+     * @return Result|string
+     */
+    public function saveImage($bucketName, $imageData, $fileName)
+    {
+        try {
+            // Save image
+            return $this->s3Client->putObject([
+                'Bucket'      => $bucketName,
+                'Key'         => $fileName,
+                'Body'        => $imageData,
+                'ContentType' => 'image/png'
+            ]);
+        } catch (AwsException $e) {
+            return 'Error: ' . $e->getAwsErrorMessage();
+        }
+    }
+}
+
